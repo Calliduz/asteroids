96e91f7f2dba93085ab6412089f31039d896c758
Calliduz <juztyned@gmail.com>
2026-05-27 19:17:04 +0800

    added dt and clock

--- main.py
+++ main.py
@@ -5,20 +5,25 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0.0
     print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
-    while True:
+    while running:
         #logging
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
+
+        dt = clock.tick(60) / 1000
+        
         screen.fill("black")
         pygame.display.flip()
-
+        
 
 if __name__ == "__main__":
     main()
